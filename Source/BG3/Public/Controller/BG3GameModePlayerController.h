511172c759bb9038e1489b53fe974f86446cf0ce
CookieAndJuice <cradiacradia@gmail.com>
2025-09-04 22:36:46 +0900

    temp: 카메라 세팅 완료. 카메라 액터로 위치 변환 구현 중

---
+++ Source/BG3/Public/Controller/BG3GameModePlayerController.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BG3GameModePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BG3_API ABG3GameModePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ABG3GameModePlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+public:	// camera
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Camera")
+	TSubclassOf<class ABG3GameCamera> BG3CameraClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	TObjectPtr<class ABG3GameCamera> BG3Camera;
+	
+	void SpawnCamera();
+
+	UFUNCTION(BlueprintCallable)
+	void SetCameraLocation(FVector location);
+};
