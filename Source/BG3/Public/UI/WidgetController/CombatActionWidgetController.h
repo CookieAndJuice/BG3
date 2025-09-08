ad9ad79d9af15d9bdf9ac4c83a7bd5294d6c1f1c
Lauren <b1327e@naver.com>
2025-09-08 18:48:48 +0900

    [Feat] 스킬 시스템과 위젯 연동 기능 구현 (1/4)

--- Source/BG3/Public/UI/WidgetController/CombatActionWidgetController.h
+++ Source/BG3/Public/UI/WidgetController/CombatActionWidgetController.h
@@ -3,10 +3,42 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
+#include "UI/Widget/ActionSlotEntry.h"
 #include "CombatActionWidgetController.generated.h"
+
+class ABaseCharacter;
+class USkillBookComponent;
+class USkillDefinition;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnActionSlotsUpdated, const TArray<FActionSlotView>&, Slots);
 
 UCLASS()
 class BG3_API UCombatActionWidgetController : public UObject
 {
 	GENERATED_BODY() 
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Combat|UI")
+	void Initialize(ABaseCharacter* InCharacter);
+
+	UFUNCTION(BlueprintCallable, Category="Combat|UI")
+	void RefreshSlots();
+
+	UPROPERTY(BlueprintAssignable, Category="Combat|UI")
+	FOnActionSlotsUpdated OnActionSlotsUpdated;
+
+private:
+	UPROPERTY()
+	TWeakObjectPtr<ABaseCharacter> OwningCharacter;
+
+	UPROPERTY()
+	USkillBookComponent* SkillBook = nullptr;
+
+	void BuildAndBroadcast();
+
+	UFUNCTION()
+	void HandleCooldownChanged(const USkillDefinition* Skill, int32 NewRounds);
+
+	UFUNCTION()
+	void HandleUsabilityChanged(const USkillDefinition* Skill, bool bUsable);
 };
