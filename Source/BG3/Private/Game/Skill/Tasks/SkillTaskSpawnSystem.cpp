58dac06df8a512e928a4c42426078b23428bd2fb
Lauren <b1327e@naver.com>
2025-09-24 11:57:36 +0900

    [Feat] ElectroBeam (for Zelda) 스킬 구현 완료

--- Source/BG3/Private/Game/Skill/Tasks/SkillTaskSpawnSystem.cpp
+++ Source/BG3/Private/Game/Skill/Tasks/SkillTaskSpawnSystem.cpp
@@ -1,16 +1,40 @@
 #include "Game/Skill/Tasks/SkillTaskSpawnSystem.h"
 
+#include "BG3/BG3.h"
 #include "Character/BaseCharacter.h"
+#include "Character/BG3PlayerCharacter.h"
+#include "Controller/BG3GameModePlayerController.h"
 #include "Game/BG3GameMode.h"
+#include "Game/BG3GameState.h"
+#include "Game/SkillExecutionSubsystem.h"
 
 void USkillTaskSpawnSystem::Start(UObject* WorldContext, AActor* Caster, const USkillDefinition* Skill,
                                   const TArray<AActor*>& Targets)
 {
-	ABG3GameMode* GM = GetWorld()->GetAuthGameMode<ABG3GameMode>();
+	PRINTLOG(TEXT("SpawnSystem Start Called"));
+	ABG3GameModePlayerController* PC = GetWorld()->GetFirstPlayerController<ABG3GameModePlayerController>();
 
 	ABaseCharacter* Character = Cast<ABaseCharacter>(Caster);
 	FVector TargetLocation = Targets[0]->GetActorLocation();
-	GM->SpawnSystem(Character, TargetLocation);
+
+	PRINTLOG(TEXT("SpawnSystem Called"));
+	PC->SpawnSystem(Character, TargetLocation);
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, [this, Targets]()
+	{
+		if (USkillExecutionSubsystem* Subsystem = GetWorld()->GetSubsystem<USkillExecutionSubsystem>())
+		{
+			ABG3GameState* GState = GetWorld()->GetGameState<ABG3GameState>();
+
+			if (ABaseCharacter* Character = Cast<ABaseCharacter>(Targets[0]))
+			{
+				Character->SetIsHit(true);
+			}
+			
+			Subsystem->FinalizeCastAfterExecutor(Targets, GState->GetCurrentRound());
+		}
+	}, 0.3f, false);
 }
 
 void USkillTaskSpawnSystem::Cancel()
